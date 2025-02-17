07379f864b9db79f1ccaf8c0433eac5d60f25a3f
Niki Dokovski <nickytd@gmail.com>
2025-02-17 10:16:32 +0100

    build: re-apply license headers

--- cmd/watcher/main.go
+++ cmd/watcher/main.go
@@ -1,6 +1,6 @@
-// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
-//
+// Copyright 2025 SAP SE or an SAP affiliate company and Gardener contributors
 // SPDX-License-Identifier: Apache-2.0
+
 package main
 
 import (
